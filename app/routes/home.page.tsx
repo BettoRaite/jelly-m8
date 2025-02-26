9525ab6eb271051ddfc6fd50d9bb69ec0350afe0
bettoraite <bettoraite@gmail.com>
2025-02-26 20:53:24 +0500

    feat(home page): Info panel, about link Info panel contains info about whether all profiles have been unlocked or not, if so then it displays a link to a secret vid

--- app/routes/home.page.tsx
+++ app/routes/home.page.tsx
@@ -20,12 +20,14 @@ import { motion } from "motion/react";
 import { ParticlesWrapper } from "@/components/ParticlesWrapper";
 import ReactConfetti from "react-confetti";
 import { FiInfo } from "react-icons/fi";
+import TypingTextEffect from "@/components/TypingText";
 export default function Home() {
   const { data: user, status, refetch } = useAuth();
   const session = useSessionMutation();
   const { data: profiles, status: profilesStatus } = useProfileQuery({
     type: "profiles",
   });
+  const [showInfoPanel, setShowInfoPanel] = useState(false);
   const handleLogout = useCallback(async () => {
     try {
       await session.mutateAsync({ type: "logout" });
@@ -67,6 +69,11 @@ export default function Home() {
   }
   const unlockedCount =
     profiles?.filter((profile) => profile.isActivated).length || 0;
+  const areAllProfilesActivated = unlockedCount === profiles?.length;
+
+  const currentDate = new Date();
+  const isMarch = currentDate.getMonth() === 2;
+  const is8thMarch = isMarch && currentDate.getDate() === 8;
 
   return (
     <div
@@ -76,29 +83,34 @@ export default function Home() {
       <span className="absolute z-10 top-32 font-caveat mx-auto py-4 flex border w-fit bg-gradient-to-r blur-xl from-blue-500 via-teal-500 to-pink-500 bg-clip-text text-8xl box-content font-extrabold text-transparent text-center select-none">
         JellyM8
       </span>
-      <motion.h1
-        animate={{
-          scale: [0, 1.1, 1],
-          y: [0, -40, 0],
-          rotate: [0, 20, -20, 0],
-          opacity: [0, 1],
-        }}
-        transition={{
-          delay: 0.4,
-        }}
-        className="pr-2 absolute z-10 top-32 font-caveat w-fit h-auto py-4 text-6xl  sm:text-8xl
-        bg-gradient-to-tr from-pink-200  to-white bg-clip-text text-transparent"
-      >
-        JellyM
-        <span className="text-pink-500 absolute rotate-[15deg] bottom-0 sm:-bottom-4 sm:-right-6">
-          8
-        </span>
-      </motion.h1>
+      <div className="absolute z-10 top-32 flex justify-center items-center flex-col">
+        <motion.h1
+          animate={{
+            scale: [0, 1.1, 1],
+            y: [0, -40, 0],
+            opacity: [0, 1],
+          }}
+          transition={{
+            delay: 0.4,
+            duration: 0.1,
+            type: "spring",
+            mass: 0.75,
+            damping: 6,
+          }}
+          className="pr-2 relative font-caveat w-fit h-auto py-4 text-6xl  sm:text-8xl
+          bg-gradient-to-tr from-pink-200  to-white bg-clip-text text-transparent"
+        >
+          JellyM
+          <span className="text-pink-500 absolute rotate-[15deg] bottom-0 sm:-bottom-4 sm:-right-6">
+            8
+          </span>
+        </motion.h1>
+      </div>
       <motion.img
         fetchPriority="high"
         animate={{
           y: [0, 2, 0, -2, 0], // Subtle vertical sway
-          x: [0, 3, 0, -3, 0], // Subtle horizontal sway
+          x: [0, 4, 0, -4, 0], // Subtle horizontal sway
           rotate: [0, 0.5, 0, -0.5, 0], // Very slight rotation
         }}
         transition={{
@@ -138,7 +150,7 @@ export default function Home() {
           })}
         </div>
       </div>
-      {/* <ReactConfetti friction={0.985} /> */}
+      {is8thMarch && <ReactConfetti friction={0.985} />}
       {user ? (
         <Button
           type="button"
@@ -162,15 +174,93 @@ export default function Home() {
         </Link>
       )}
       <Toaster />
-      <div className="font-jost w-52 h-52 absolute bottom-10 bg-white bg-opacity-20 border border-white  right-20 rounded-xl p-4">
-        <p className="text-white text-sm text-center">Профилей активированно</p>
-        <p className="text-white text-sm text-center">
-          {unlockedCount}/{profiles?.length ?? 0}
-        </p>
-      </div>
-      <Button className="scale-90 absolute z-10 bottom-4 right-4 rounded-[100%] ">
+      {/* Info panel */}
+      {showInfoPanel && (
+        <motion.div
+          initial={{ opacity: 0, scale: 0.5, y: 100, x: 100 }}
+          animate={{ opacity: 1, scale: 1, y: 0, x: 0 }}
+          className="font-jost w-64 h-64 absolute bottom-10 bg-white bg-opacity-20 backdrop-blur-sm border-2 border-white border-opacity-30 right-20 rounded-xl p-6 shadow-lg flex flex-col justify-center items-center space-y-4"
+        >
+          {!areAllProfilesActivated && (
+            <>
+              <p className="text-white text-lg text-center font-bold">
+                Осталось активировать профилей
+              </p>
+              <p className="text-white text-2xl text-center font-semibold">
+                {unlockedCount}/{profiles?.length ?? 0}
+              </p>
+            </>
+          )}
+          {areAllProfilesActivated && (
+            <p className="text-white text-[1rem] text-center font-bold">
+              Поздравляю вы активировали все профили 🥳
+            </p>
+          )}
+          {/* Video Link */}
+          {areAllProfilesActivated && (
+            <Link
+              to="https://drive.google.com/file/d/1v8sNHC4vEcWHi7Y6yrYcBgCd_WVzuQpH/view?usp=drive_link" // Replace with your video link
+              target="_blank"
+              rel="noopener noreferrer"
+              className="mt-4 inline-flex items-center justify-center px-4 py-2 bg-white bg-opacity-30 hover:bg-opacity-50 text-white text-sm font-semibold rounded-lg transition-all duration-300 ease-in-out transform hover:scale-105"
+            >
+              Watch Video
+              <svg
+                className="w-4 h-4 ml-2"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M14.752 11.168l-3.197-2.132A1 1 0 0010 9.87v4.263a1 1 0 001.555.832l3.197-2.132a1 1 0 000-1.664z"
+                />
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                />
+              </svg>
+            </Link>
+          )}
+        </motion.div>
+      )}
+      {/* cta text */}
+      {areAllProfilesActivated && !showInfoPanel && (
+        <motion.div
+          className="z-10 bottom-5 md:bottom-6 md:right-20 right-14 absolute"
+          animate={{
+            scale: [0.9, 1, 0.9],
+          }}
+          transition={{
+            delay: 1,
+            duration: 2,
+            repeat: Number.POSITIVE_INFINITY,
+          }}
+        >
+          <TypingTextEffect
+            className="font-caveat text-white text-sm md:text-lg"
+            text="Кажись тут что-то есть 👉"
+          />
+        </motion.div>
+      )}
+      {/* toggle info panel btn */}
+      <Button
+        onClick={() => setShowInfoPanel((s) => !s)}
+        className="scale-90 absolute z-10 bottom-4 right-4 rounded-[100%]"
+      >
         <FiInfo className="text-white md:my-2" />
       </Button>
+      <Link
+        to={"/about"}
+        className="absolute bottom-3 left-3 text-slate-300 text-opacity-30"
+      >
+        Made by bettoraite
+      </Link>
     </div>
   );
 }
